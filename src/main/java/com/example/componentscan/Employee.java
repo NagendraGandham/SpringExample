5bbaec2e77ad761a0098e92d90439b892fc81bbe
Nagendra Gandham <nagendragandham770@gmail.com>
2026-07-08 21:33:28 +0530

    annotaions component value

---
+++ src/main/java/com/example/componentscan/Employee.java
@@ -0,0 +1,56 @@
+package com.example.componentscan;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Employee {
+    private int employeeId;
+    @Value("Hello")
+    private String firstName;
+    @Value("${java.home}")
+    private String lastName;
+    private double salary;
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId=" + employeeId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
